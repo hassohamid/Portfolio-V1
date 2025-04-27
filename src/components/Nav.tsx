1bc7bff975f9277757917accb0f58a2d00e2c5e2
hassohamid <hassohamid@hotmail.com>
2025-04-27 22:46:32 +0200

    DropdownMenu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,54 @@
+import { Button } from "@/components/ui/button";
+import { ArrowDown } from "lucide-react";
+import { useTheme } from "@/theme/ThemeProvider";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 export default function Nav() {
+  const { theme } = useTheme();
+
   return (
     <>
-      <div>
-        <h1> NAV </h1>
-      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button className="cursor-pointer" variant="ghost">
+            <ArrowDown />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            {theme === "dark" ? "🖤" : "🤍"}
+          </DropdownMenuLabel>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem asChild>
+            <a href="#about" className="cursor-pointer">
+              About
+            </a>
+          </DropdownMenuItem>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem asChild>
+            <a href="#stack" className="cursor-pointer">
+              Stack
+            </a>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <a href="#projects" className="cursor-pointer">
+              Projects
+            </a>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 }
