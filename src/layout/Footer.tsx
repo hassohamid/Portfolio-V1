93297279c85ffeee09c660ce4a5a6ac3162c04b6
hassohamid <hassohamid@hotmail.com>
2025-04-28 04:39:18 +0200

    Footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -5,14 +5,15 @@ export function Footer() {
     <footer className="py-6 border-t border-border/40 bg-background/70 backdrop-blur-sm">
       <div className="container  mx-auto px-6">
         <p className="flex justify-center items-center gap-1 text-center text-sm text-muted-foreground/80">
-          made with <Heart size={12} /> by
+          &lt; crafted with <Heart size={12} /> by
           <a
             href="https://github.com/hassohamid"
             target="_blank"
             className="text-primary hover:text-primary/80 "
           >
-            hassohamid
+            hasso
           </a>
+          /&gt;
         </p>
       </div>
     </footer>
