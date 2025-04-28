397a348f94458d0a2c816d22748b6082d42b6f5a
hassohamid <hassohamid@hotmail.com>
2025-04-28 03:44:22 +0200

    Desktop Version Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { ArrowDown } from "lucide-react";
 import { useTheme } from "@/theme/ThemeProvider";
+import { ModeToggle } from "@/theme/ModeToggle";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,41 +15,56 @@ export default function Nav() {
   const { theme } = useTheme();
 
   return (
-    <>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Button className="cursor-pointer !transition-none" variant="ghost">
-            <ArrowDown />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>
-            {theme === "dark" ? "🖤" : "🤍"}
-          </DropdownMenuLabel>
+    <nav className="flex items-center gap-4">
+      <div className="hidden md:flex items-center gap-4">
+        <a href="#about" className="hover:text-primary relative py-1 group">
+          About
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-primary group-hover:w-full transition-all duration-300 ease-in-out"></span>
+        </a>
+        <a href="#stack" className="hover:text-primary relative py-1 group">
+          Stack
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-primary group-hover:w-full transition-all duration-300 ease-in-out"></span>
+        </a>
+        <a href="#projects" className="hover:text-primary relative py-1 group">
+          Projects
+          <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-primary group-hover:w-full transition-all duration-300 ease-in-out"></span>
+        </a>
+      </div>
 
-          <DropdownMenuSeparator />
+      <div className="md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button className="cursor-pointer !transition-none" variant="ghost">
+              <ArrowDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              {theme === "dark" ? "🖤" : "🤍"}
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <a href="#about" className="cursor-pointer">
+                About
+              </a>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <a href="#stack" className="cursor-pointer">
+                Stack
+              </a>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <a href="#projects" className="cursor-pointer">
+                Projects
+              </a>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
 
-          <DropdownMenuItem asChild>
-            <a href="#about" className="cursor-pointer">
-              About
-            </a>
-          </DropdownMenuItem>
-
-          <DropdownMenuSeparator />
-
-          <DropdownMenuItem asChild>
-            <a href="#stack" className="cursor-pointer">
-              Stack
-            </a>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
-            <a href="#projects" className="cursor-pointer">
-              Projects
-            </a>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
+      <ModeToggle />
+    </nav>
   );
 }
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,14 +1,10 @@
-import { ModeToggle } from "../theme/ModeToggle";
 import Nav from "@/components/Nav";
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 flex justify-between items-center px-6 py-4 backdrop-blur-md bg-background/70 border-b border-border/40  ">
       <div className="font-bold text-xl">Portfolio.</div>
-      <div className="flex items-center gap-4">
-        <Nav />
-        <ModeToggle />
-      </div>
+      <Nav />
     </header>
   );
 }
