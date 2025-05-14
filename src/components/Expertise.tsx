14a8090a642f455c6a5d31d8c368c3d7dd17fcd2
hassohamid <hassohamid@hotmail.com>
2025-05-14 08:07:23 +0200

    OptimizedMarquee

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -5,8 +5,6 @@ export default function Expertise() {
   return (
     <Marquee
       autoFill
-      gradient
-      gradientWidth={70}
       speed={30}
       gradientColor="#393939
 "
@@ -14,28 +12,28 @@ export default function Expertise() {
     >
       <div
         className="flex
-       gap-10 text-muted-foreground p-3 items-center text-xs tracking-wide "
+       gap-10 text-muted-foreground/60 p-3 items-center text-xs tracking-wider "
       >
         <h1 className="hover:border-b-2 "> USER FRIENDLY </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> RESPONSIVE </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> MINIMALIST </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> INTERACTIVE </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> PERFOMANT </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> MODERN </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> SECURE </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> SCALABLE </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> MAINTAINABLE </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1> SEARCH OPTIMIZED </h1>
-        <Diamond size={15} strokeWidth={1} />
+        <Diamond size={13} strokeWidth={1} />
         <h1></h1>
       </div>
     </Marquee>
