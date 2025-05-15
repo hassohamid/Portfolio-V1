831ca012d6440f469f06c8579c43d3a3a50b5f79
hassohamid <hassohamid@hotmail.com>
2025-05-15 10:07:09 +0200

    Resp

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -67,7 +67,7 @@ export default function SplashScreen({
     >
       <h1
         ref={textRef}
-        className="text-white/60 font-black text-7xl  tracking-wider"
+        className="text-white/60 font-black text-xl sm:text-5xl  tracking-wider"
       ></h1>
     </div>
   );
