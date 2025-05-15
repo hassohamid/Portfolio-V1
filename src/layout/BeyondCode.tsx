6784c6af962614398889c2f2ebae0b3af628aab0
hassohamid <hassohamid@hotmail.com>
2025-05-15 04:49:30 +0200

    Refinement

--- src/layout/BeyondCode.tsx
+++ src/layout/BeyondCode.tsx
@@ -12,13 +12,14 @@ import { Interests } from "@/data/Interests";
 import { motion } from "framer-motion";
 import { Badge } from "@/components/ui/badge";
 
+import { FaRegCommentAlt } from "react-icons/fa";
+
 export default function BeyondCode() {
   return (
-    <section className="py-20 bg-[#f8f8f8] dark:bg-[#111111] ">
+    <section id="BeyondCode" className="py-20 bg-[#f8f8f8] dark:bg-[#111111] ">
       <div className="flex flex-col items-center">
         <h1 className="mb-4 text-sm text-muted-foreground/50 tracking-wider">
-          {" "}
-          MY LIFE{" "}
+          MY LIFE
         </h1>
         <h1 className="text-3xl text-center font-bold mb-4 flex items-center gap-1  ">
           Beyond the <span className="text-muted-foreground/70  ">Code</span>
@@ -30,26 +31,21 @@ export default function BeyondCode() {
       </div>
 
       <div className="container max-w-8xl mx-auto ">
-        <div className="absolute top-0 right-5 z-10 p-2 ">
-          <ScanFace size={20} className="text-primary/50 hover:text-primary" />
-        </div>
-
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
           className="flex flex-col sm:flex-row gap-4 p-4 sm:justify-center"
         >
-          {" "}
           <Card className="w-full sm:w-2/3 rounded-lg border border-border/20 shadow-sm backdrop-blur-sm bg-background/80">
-            <CardHeader className="pb-4">
+            <CardHeader className="relative">
               <motion.div
                 initial={{ opacity: 0, y: -10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.1, duration: 0.5 }}
               >
                 <CardTitle className="text-2xl  tracking-widest ">
-                  <Badge className="p-2 dark:bg-yellow-500">
+                  <Badge className="p-2 bg-teal-400 text-black">
                     GET TO KNOW ME
                   </Badge>
                 </CardTitle>
@@ -57,6 +53,10 @@ export default function BeyondCode() {
                   Who am I outside the techy world?
                 </CardDescription>
               </motion.div>
+              <FaRegCommentAlt
+                className="text-primary/70 absolute top-3 right-6 "
+                size={24}
+              />
             </CardHeader>
             <CardContent>
               <motion.div
@@ -84,30 +84,28 @@ export default function BeyondCode() {
             </CardFooter>
           </Card>
           <Card className="w-full rounded-lg border border-border/20 shadow-sm backdrop-blur-sm bg-background/80">
-            <CardHeader className="relative pb-6">
-              <motion.div
-                className="absolute top-0 right-6"
-                initial={{ opacity: 0, x: 20 }}
-                animate={{ opacity: 1, x: 0 }}
-                whileHover={{ rotate: 15, scale: 1.1 }}
-                transition={{ type: "spring", stiffness: 300, damping: 10 }}
-              >
-                <ScanFace className="text-primary/70" size={24} />
-              </motion.div>
+            <CardHeader className="relative ">
+              <ScanFace
+                className="text-primary/70 absolute top-3 right-6"
+                size={24}
+              />
+
               <motion.div
                 initial={{ opacity: 0, y: -10 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.2, duration: 0.5 }}
               >
-                <CardTitle className="text-2xl text-foreground/90 tracking-wider font-bold">
-                  Hobbies & Interests
+                <CardTitle className="text-2xl text-foreground/90 tracking-widest font-bold">
+                  <Badge className="p-2 bg-teal-400 text-black">
+                    HOBBIES & INTERESTS
+                  </Badge>
                 </CardTitle>
                 <CardDescription className="mt-2">
                   A big part of my life consists of
                 </CardDescription>
               </motion.div>
             </CardHeader>
-            <CardContent className="grid grid-cols-2 sm:grid-cols-3 gap-4 p-4">
+            <CardContent className="grid grid-cols-4 sm:grid-cols-4 gap-4 p-4">
               {Interests.map((interest, index) => {
                 return (
                   <motion.div
@@ -143,14 +141,6 @@ export default function BeyondCode() {
               })}
             </CardContent>
           </Card>{" "}
-        </motion.div>
-        <motion.div
-          className="p-4"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.8 }}
-        >
-          <Card className="rounded-lg border border-border/20 shadow-sm backdrop-blur-sm bg-background/80"></Card>
         </motion.div>
       </div>
     </section>
