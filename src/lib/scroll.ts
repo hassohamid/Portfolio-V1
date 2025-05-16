932572f390679852b7729be8e4a0230621faf2e9
hassohamid <hassohamid@hotmail.com>
2025-05-16 10:26:27 +0200

    TS Error

--- src/lib/scroll.ts
+++ src/lib/scroll.ts
@@ -4,9 +4,9 @@ export function SmoothScroll() {
   const lenis = new Lenis({
     syncTouch: true,
     anchors: true,
-  });
+  } as any);
 
-  function raf(time) {
+  function raf(time: number) {
     lenis.raf(time);
     requestAnimationFrame(raf);
   }
