156f69274ec161da7b4b700e7fa2208a595432ec
hassohamid <hassohamid@hotmail.com>
2025-05-16 11:50:44 +0200

    MacScrolling

--- src/lib/scroll.ts
+++ src/lib/scroll.ts
@@ -1,8 +1,13 @@
 import Lenis from "@studio-freight/lenis";
 
 export function SmoothScroll() {
+  const isMac = /Mac/i.test(navigator.userAgent);
+
+  if (isMac) return;
+
   const lenis = new Lenis({
     anchors: true,
+    syncTouch: false,
   } as any);
 
   function raf(time: number) {
