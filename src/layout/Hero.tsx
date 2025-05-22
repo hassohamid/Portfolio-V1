285e77c8555897a927a39a8982dd47fe5807ede1
hassohamid <hassohamid@hotmail.com>
2025-05-22 08:08:26 +0200

    Avatar to WEBP

--- src/layout/Hero.tsx
+++ src/layout/Hero.tsx
@@ -25,7 +25,12 @@ export default function Hero() {
           <div className="dark:bg-background/50 backdrop-blur-xs sm:border-l p-6 md:p-8  ">
             <div className="flex flex-col sm:flex-row items-center gap-8 relative ">
               <Avatar className="size-32 sm:size-36 ring-2 ring-white/10 ring-offset-2 ring-offset-background hover:ring-primary/20 transition-all duration-200  ">
-                <AvatarImage src="8.jpg" alt="HASSO" />
+                <AvatarImage
+                  src="Goku.webp"
+                  alt="HASSO"
+                  loading="eager"
+                  className="w-full h-full object-cover"
+                />
                 <AvatarFallback>HH</AvatarFallback>
               </Avatar>
 
