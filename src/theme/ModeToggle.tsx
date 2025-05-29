02212758b1033e2347e7eabf8b1bf925972ecc3a
hassohamid <hassohamid@hotmail.com>
2025-05-29 22:48:19 +0200

    noTransition

--- src/theme/ModeToggle.tsx
+++ src/theme/ModeToggle.tsx
@@ -13,8 +13,8 @@ export function ModeToggle() {
       className="relative cursor-pointer p-2 hover:opacity-70 "
       onClick={toggleTheme}
     >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute top-2 left-2 h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100  dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute top-2 left-2 h-[1.2rem] w-[1.2rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
     </div>
   );
 }
