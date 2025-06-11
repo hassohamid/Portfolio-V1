f0d0d37036962e815e449f06a152e6200a50b34b
hassohamid <hassohamid@hotmail.com>
2025-06-11 23:18:31 +0200

    DeletedSplashScreen

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import Stack from "@/layout/Stack";
 import Projects from "@/layout/Projects";
 import { Toaster } from "./components/ui/sonner";
 import BeyondCode from "@/layout/BeyondCode";
-// import { useState } from "react";
-// import SplashScreen from "./components/SplashScreen";
 import { useEffect } from "react";
 import LocomotiveScroll from "locomotive-scroll";
 
@@ -23,8 +21,6 @@ export default function App() {
 
   return (
     <>
-      {/* {showSplash && <SplashScreen onComplete={() => setShowSplash(false)} />} */}
-
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="min-h-screen bg-background transition-colors ">
           <Header />
