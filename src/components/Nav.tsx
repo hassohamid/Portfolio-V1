a74b8c16346f4625d9aafa0fc5a47b286aaa6a18
hassohamid <hassohamid@hotmail.com>
2025-06-12 23:39:21 +0200

    primary

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,7 +8,7 @@ export default function Nav() {
         <WordRotate
           duration={4000}
           words={["For inquiries", "Reach out"]}
-          className=" flex items-center gap-1  uppercase tracking-tighter text-xs hover:text-primary/80 border-b-1 border-dotted "
+          className=" flex items-center gap-1  uppercase tracking-tighter text-xs hover:text-primary/80 border-b-1 border-dotted dark:border-primary/30"
         />
       </a>
       <ModeToggle />
