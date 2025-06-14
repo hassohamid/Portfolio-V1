a1a470801a55546935be080efa08ca150d0a77e0
hassohamid <hassohamid@hotmail.com>
2025-06-14 05:14:04 +0200

    QASection

---
+++ src/components/ui/cornerframe.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+export function CornerFrame({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={cn("relative px-2 ", className)}>
+      <div className="absolute top-0 left-0 w-4 h-4 border-t-1 border-l-1 border-border  " />
+      <div className="absolute top-0 right-0 w-4 h-4 border-t-1 border-r-1 border-border " />
+      <div className="absolute bottom-0 left-0 w-4 h-4 border-b-1 border-l-1 border-border " />
+      <div className="absolute bottom-0 right-0 w-4 h-4 border-b-1 border-r-1 border-border" />
+      {children}
+    </div>
+  );
+}
--- src/layout/BeyondCode.tsx
+++ src/layout/BeyondCode.tsx
@@ -1,22 +1,13 @@
-import { PiBracketsAngleFill } from "react-icons/pi";
-import {
-  Card,
-  CardTitle,
-  CardContent,
-  CardDescription,
-  CardHeader,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { ScanFace } from "lucide-react";
-import { Interests } from "@/data/Interests";
-import { Badge } from "@/components/ui/badge";
-import { FaRegCommentAlt } from "react-icons/fa";
+
 import { motion } from "motion/react";
+import { CornerFrame } from "@/components/ui/cornerframe";
 
 export default function BeyondCode() {
   return (
@@ -27,90 +18,25 @@ export default function BeyondCode() {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
         >
-          <div className="flex flex-col items-center pb-10 pt-8">
-            <h1 className="mb-4 text-sm text-muted-foreground/50 tracking-widest ">
-              Q&A
-            </h1>
-            <h1 className="text-3xl   text-center font-bold mb-4 flex items-center gap-1 ">
-              Beyond the
-              <span className="text-muted-foreground/70  ">Code</span>
-              <PiBracketsAngleFill />
-            </h1>
-          </div>
-
-          <div className="container max-w-3xl mx-auto ">
-            <div className="flex flex-col sm:flex-row gap-4 p-4 sm:justify-center  ">
-              <Card className="sm:hidden w-full rounded-lg border border-border/20 shadow-sm backdrop-blur-sm bg-background/80">
-                <CardHeader className="relative ">
-                  <ScanFace
-                    className="text-primary/70 absolute top-3 right-6"
-                    size={22}
-                  />
-
-                  <CardTitle className="text-xl text-foreground/90 tracking-widest font-bold">
-                    <Badge className="p-2  font-bold ">
-                      HOBBIES & INTERESTS
-                    </Badge>
-                  </CardTitle>
-                  <CardDescription className="mt-2">
-                    A big part of my life consists of
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="grid grid-cols-3 sm:grid-cols-2 gap-4 p-4">
-                  {Interests.map((interest, index) => {
-                    return (
-                      <motion.div
-                        key={index}
-                        className="flex items-center flex-col justify-center p-4 rounded-xl 
-                  bg-gradient-to-br from-background/90 to-background 
-                  border border-border/30 shadow-sm hover:shadow-md 
-                  hover:border-primary/30 transition-all duration-300"
-                        whileHover={{
-                          y: -5,
-                          transition: { duration: 0.2 },
-                        }}
-                      >
-                        <motion.div
-                          className="text-primary/70 mb-2"
-                          whileHover={{ scale: 1.2, rotate: 5 }}
-                          transition={{
-                            type: "spring",
-                            stiffness: 400,
-                            damping: 10,
-                          }}
-                        >
-                          <interest.icon size={29} />
-                        </motion.div>
-                        <h3 className="text-sm font-medium text-foreground/90">
-                          {interest.interest}
-                        </h3>
-                      </motion.div>
-                    );
-                  })}
-                </CardContent>
-              </Card>
-
-              <Card className="w-full rounded-lg border border-border/20 shadow-sm backdrop-blur-sm bg-background/80">
-                <CardHeader className="relative">
-                  <CardTitle className="text-xl  tracking-widest ">
-                    <Badge className="p-2   font-bold ">GET TO KNOW ME</Badge>
-                  </CardTitle>
-                  <CardDescription className="mt-2">
-                    Who am I outside the techy world?
-                  </CardDescription>
-
-                  <FaRegCommentAlt
-                    className="text-primary/70 absolute top-3 right-6 "
-                    size={22}
-                  />
-                </CardHeader>
+          <div className="container max-w-3xl mx-auto px-8 md:px-0  ">
+            <div className="flex justify-center mb-4">
+              <h1 className=" text-center  text-sm text-muted-foreground/50 tracking-widest">
+                Q&A
+              </h1>
+            </div>
+            <CornerFrame>
+              <Card className="w-full border-none shadow-none backdrop-blur-sm bg-transparent">
                 <CardContent>
-                  <Accordion type="single" collapsible>
+                  <Accordion
+                    type="single"
+                    collapsible
+                    className="text-muted-foreground "
+                  >
                     <AccordionItem value="item 1">
                       <AccordionTrigger className="flex items-center cursor-pointer">
                         My go-to productivity hack?
                       </AccordionTrigger>
-                      <AccordionContent className="leading-relaxed tracking-tight ">
+                      <AccordionContent className="leading-relaxed tracking-tight  ">
                         I might sound crazy but when I feel like I'm falling
                         behind, I drink half a Redbull and nap for 15-30 min.
                         It's actually a real thing, called "The Coffee Nap".
@@ -164,10 +90,27 @@ export default function BeyondCode() {
                         Beating 3km under 12 minutes.
                       </AccordionContent>
                     </AccordionItem>
+                    <AccordionItem value="item 7">
+                      <AccordionTrigger className="cursor-pointer">
+                        What do I like to do during free-time?
+                      </AccordionTrigger>
+                      <AccordionContent className="leading-relaxed tracking-tight ">
+                        Sports-lover. All kind of sports. Grew up with football,
+                        fell in love with Tennis.
+                      </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item 8">
+                      <AccordionTrigger className="cursor-pointer">
+                        My biggest regret?
+                      </AccordionTrigger>
+                      <AccordionContent className="leading-relaxed tracking-tight ">
+                        Not getting into web developing earlier.
+                      </AccordionContent>
+                    </AccordionItem>
                   </Accordion>
                 </CardContent>
               </Card>
-            </div>
+            </CornerFrame>
           </div>
         </motion.div>
       </section>
