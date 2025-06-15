d21980339e99d8178a09804a6ea082177aa77a1b
hassohamid <hassohamid@hotmail.com>
2025-06-15 21:33:53 +0200

    HeaderBorder

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,9 +3,11 @@ import Logo from "@/components/Logo";
 
 export function Header() {
   return (
-    <header className="backdrop-blur-xs flex justify-between container max-w-3xl mx-auto px-5 pt-3 sm:pt-2  ">
-      <Logo />
-      <Contact />
+    <header className=" border-b ">
+      <div className="flex max-w-3xl mx-auto px-5 pt-2 justify-between">
+        <Logo />
+        <Contact />
+      </div>
     </header>
   );
 }
