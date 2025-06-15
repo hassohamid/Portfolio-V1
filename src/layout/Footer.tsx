c800548cceabcf681054eab20b3e7e2f6b94fa04
hassohamid <hassohamid@hotmail.com>
2025-06-15 06:15:56 +0200

    FooterUpdated

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,21 +1,10 @@
-import { Heart } from "lucide-react";
-
 export function Footer() {
   return (
     <footer className="py-3 border-t border-border/40 bg-background/70 backdrop-blur-sm">
-      <div className="container mx-auto  px-6">
-        <p className="flex justify-center items-center gap-1 text-center tracking-tighter text-sm text-muted-foreground/60">
-          &lt; made with <Heart size={12} strokeWidth={1.5} /> by
-          <a
-            href="https://github.com/hassohamid"
-            target="_blank"
-            className="hover:text-primary/80 "
-          >
-            hasso
-          </a>
-          /&gt;
-        </p>
-      </div>
+      <h1 className="text-center uppercase text-xs tracking-tight text-primary/90 ">
+        {" "}
+        © Hasso Hamid{" "}
+      </h1>
     </footer>
   );
 }
