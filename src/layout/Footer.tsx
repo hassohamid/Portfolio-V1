b974dc98607a783dfceb89f27653c58e7798062a
hassohamid <hassohamid@hotmail.com>
2025-06-16 08:59:27 +0200

    FooterBorder

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="py-3 border-t border-border/40 border-dashed bg-background/70 backdrop-blur-sm">
+    <footer className="py-3 border-t border-border/60 border-dashed bg-background/70 backdrop-blur-sm">
       <h1 className="text-center uppercase text-xs tracking-tight text-primary/60 ">
         {" "}
         © Hasso Hamid{" "}
